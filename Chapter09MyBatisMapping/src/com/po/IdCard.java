bda6f3b037fc285a489365d1523153c6fdaff94e
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-05-12 20:39:40 +0800

    Chapter09MyBatisMapping

---
+++ Chapter09MyBatisMapping/src/com/po/IdCard.java
@@ -0,0 +1,19 @@
+package com.po;
+
+public class IdCard {
+	private Integer id;
+	private Integer code;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCode() {
+		return code;
+	}
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+	
+}
