3e9aecd9cc306784ee9784726d35f1715b36f172
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-06-01 10:23:53 +0800

    SpringMVC CORE

---
+++ Chapter12CoreClassAndAnno/src/com/controller/FirstController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+/*
+ * 控制器类
+ */
+@Controller
+@RequestMapping(value="/hello")
+public class FirstController {
+	@RequestMapping(value="/firstController")
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+		//向模型对象对象中添加数据
+		model.addAttribute("msg", "这就是我的第一个Spring MVC程序");
+		//返回视图页面
+		return "first";
+	}
+}
